28ab32775e17535a794c3e52fd2266bb786070bf
tanken <tan.kenson@gmail.com>
2016-03-30 12:38:32 +0800

    Categorisation

--- process_json.py
+++ process_json.py
@@ -12,15 +12,75 @@ if __name__ == "__main__":
 	length_all = len(confessions)
 
 	count = {}		# dictionary
+	count_confession = {}
 	time_day = {}
 	date_day = {}
 
-	for confession in confessions:
-	#for confession in confessions[0:120]:
+
+	# Categories
+	health = ['insomia', 'sleep', 'upset', 'healthy', 'distress', 'feeling', 'depression', 'destress', 'UHC', 'gym', 'fit', 'idenitfy', 'esteem', 'introvert', 'introverts']
+	relationship = []
+	education = []
+
+	health_score = 0
+	relationship_score = 0
+	education_score = 0
+
+	with open('categorisation.csv', 'wb') as fp:
+		a = csv.writer(fp, delimiter=',')
+		a.writerow(('Id', 'Health', 'Relationship', 'Education', 'Most'))
+		#a.writerows(sorted_count[0:10]) # top 10
+		#a.writerows(sorted_time_day)
+
+		#for confession in confessions:
+		for confession in confessions[0:500]:
+			date_time = confession["created_at"]
+			postID = confession["confession_id"]
+			
+			content = confession["content"]			# categorisation
+
+			for word in health:
+				#print word
+				if word in content:
+					#print True
+					health_score += 1
+
+			for word in relationship:
+				#print word
+				if word in content:
+					#print True
+					relationship_score += 1
+
+			for word in health:
+				#print word
+				if word in education:
+					#print True
+					education_score += 1
+
+			most = 'Others'
+			if ((health_score > 0) or (relationship_score > 0) or (education_score > 0)):
+				if(health_score == max(health_score, relationship_score, education_score)):
+					most = 'Health'
+				elif(relationship_score == max(health_score, relationship_score, education_score)):
+					most = 'Relationship'
+				else:
+					most = 'Education'
+
+
+			a.writerow((postID, health_score, relationship_score, education_score, most))
+			health_score = 0
+			education_score = 0
+			relationship_score = 0
+
+			#print health_score
+
+	#for confession in confessions:
+	"""for confession in confessions[0:120]:
 		date_time = confession["created_at"]
 		postID = confession["confession_id"]
 		
-		"""content = confession["content"]		# Unique Words
+
+		content = confession["content"]		# Unique Words
 		#print confession["confession_id"]
 
 		#for w in content.split():
@@ -28,10 +88,19 @@ if __name__ == "__main__":
 			#print w
 			w = w.strip(';.:!,()')
 			w = w.lower()
+			if w in count_confession:
+				count_confession[w] = 1
+			else:
+				count_confession[w] = 1
+
+		for w in count_confession:
+			#print w
 			if w in count:
 				count[w] += 1
 			else:
-				count[w] = 1"""
+				count[w] = 1
+
+		count_confession = {}
 
 		temp = date_time.split('T')
 		boolly = False
@@ -65,19 +134,19 @@ if __name__ == "__main__":
 	# === Unique Hour ===
 	# ===================
 
-	"""sorted_time_day = sorted(time_day.items(), key=operator.itemgetter(0))
+	sorted_time_day = sorted(time_day.items(), key=operator.itemgetter(0))
 	
 	with open('hour_counts.csv', 'wb') as fp:
 		a = csv.writer(fp, delimiter=',')
 		a.writerow(('Hour', 'Times'))
 		#a.writerows(sorted_count[0:10]) # top 10
-		a.writerows(sorted_time_day)"""
+		a.writerows(sorted_time_day)
 
 	# ====================
 	# === Unique Words ===
 	# ====================
 
-	"""# dictionary converted to a list
+	# dictionary converted to a list
 	sorted_count = sorted(count.items(), key=operator.itemgetter(1), reverse=True)
 
 	#for word, times in sorted_count:
